02159433b92faded47dd9d477b36f173f8a71a40
Viktoriia Koloda <kassy1323@gmail.com>
2026-03-06 03:43:31 +0100

    add categories

---
+++ src/client/components/Categories/Categories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as styles from './Categories.module.scss';
+import { useSelector } from 'react-redux';
+import WidthWrapper from '../WidthWrapper/WidthWrapper.js';
+import ButtonLink from '../ButtonLink/ButtonLink.js';
+import { useNavigate } from 'react-router-dom';
+
+const Categories = () => {
+    const { categories } = useSelector(store => store.products);
+    const navigate = useNavigate();
+    const handleClick = (category) => {
+        navigate(`/products/${category}`);
+    }
+    return (
+        <WidthWrapper className={styles.categories}>
+            {categories?.map(({ name, description, imageUrl, id }) =>
+            (<div key={id} className={styles.category} onClick={() => handleClick(name)}>
+                <img key={id} src={imageUrl} />
+                <div className={styles.textWrapper}>
+                    <h2 className={styles.title}>{name}</h2>
+                    <h3 className={styles.description}>{description}</h3>
+                </div>
+            </div>)
+            )}
+        </WidthWrapper >
+    )
+}
+
+export default Categories;
--- src/client/pages/Landing/Landing.js
+++ src/client/pages/Landing/Landing.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import * as styles from './Landing.module.scss';
 import { useSelector } from 'react-redux';
-import ProductItem from '../../components/ProductItem/ProductItem.js';
-import ProductsList from '../../components/ProductsList/ProductsList.js';
 import Hero from '../../components/Hero/Hero.js';
+import Categories from '../../components/Categories/Categories.js';
 
 const Landing = () => {
     const { products } = useSelector(state => state.products)
     return (
         <div className={styles.landing}>
             <Hero />
-            <ProductsList products={products} />
+            <Categories />
         </div>
     )
 }
