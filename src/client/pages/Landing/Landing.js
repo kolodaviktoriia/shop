1ec8543f6b526edd16ec70297ac6f96691f67bfa
Viktoriia Koloda <kassy1323@gmail.com>
2026-03-09 03:43:15 +0100

    fix render

--- src/client/pages/Landing/Landing.js
+++ src/client/pages/Landing/Landing.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import * as styles from './Landing.module.scss';
-import { useSelector } from 'react-redux';
 import Hero from '../../components/Hero/Hero.js';
 import Categories from '../../components/Categories/Categories.js';
 import Collections from '../../components/Collections/Collections.js';
 
 const Landing = () => {
-    const { products } = useSelector(state => state.products)
     return (
-        <div className={styles.landing}>
+        <div>
             <Hero />
             <Categories />
             <Collections />
