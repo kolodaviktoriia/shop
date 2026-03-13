604d4d3c15449d013df888c6b0a426c6d5436ed0
Viktoriia Koloda <kassy1323@gmail.com>
2026-03-13 17:59:40 +0100

    add pageWrapper

---
+++ src/client/components/PageWrapper/PageWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as styles from './PageWrapper.module.scss';
+
+const PageWrapper = ({ children }) => {
+  return (
+    <div
+      className={styles.pageWrapper}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default PageWrapper;
--- src/client/pages/About/About.js
+++ src/client/pages/About/About.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Message from '../../components/Message/Message.js';
-
-import * as styles from './About.module.scss';
+import PageWrapper from '../../components/PageWrapper/PageWrapper.js';
 
 const About = () => {
   return (
-    <div className={styles.notFound}>
+    <PageWrapper>
       <Message
         title="About Us"
         subtitle={`Blush & Bloom is a cosmetics shop inspired by nature and soft beauty. Our goal is to create gentle products that help you feel confident and radiant every day.`}
       />
-    </div>
+    </PageWrapper>
   );
 };
 
