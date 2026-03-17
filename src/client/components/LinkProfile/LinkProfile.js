fcd33b4479c11572c5618783184018c6e5d33573
Viktoriia Koloda <kassy1323@gmail.com>
2026-03-17 04:59:22 +0100

    fix log out btn

--- src/client/components/LinkProfile/LinkProfile.js
+++ src/client/components/LinkProfile/LinkProfile.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 import * as styles from './LinkProfile.module.scss';
 
-const LinkProfile = ({ to, children, className }) => {
+const LinkProfile = ({ to, children, className, onClick }) => {
   const linkClasses = `${styles.linkProfile} ${className ?? ''}`;
-  return (
+  return onClick ? (
+    <button className={linkClasses} onClick={onClick}>
+      {children}
+    </button>
+  ) : (
     <Link to={to} className={linkClasses}>
       {children}
     </Link>
