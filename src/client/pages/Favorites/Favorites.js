81dfbe2b5e44040d442c40a973cea99ad2bbc223
Viktoriia Koloda <kassy1323@gmail.com>
2026-03-17 05:35:22 +0100

    add message to favorites

--- src/client/pages/Favorites/Favorites.js
+++ src/client/pages/Favorites/Favorites.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFavorites } from '../../slices/productsSlice.js';
 import ProductsList from '../../components/ProductsList/ProductsList.js';
-import WidthWrapper from '../../components/WidthWrapper/WidthWrapper.js';
 import Spinner from '../../components/Spinner/Spinner.js';
+import Message from '../../components/Message/Message.js';
 
 import * as styles from './Favorites.module.scss';
 
@@ -16,10 +16,20 @@ const Favorites = () => {
   }, [dispatch]);
 
   return (
-    <WidthWrapper>
+    <div className={styles.favorites}>
       <h1 className={styles.title}>Your Favorites</h1>
-      {loading ? <Spinner /> : <ProductsList products={favorites} />}
-    </WidthWrapper>
+      {loading ? (
+        <Spinner />
+      ) : !favorites || favorites.length === 0 ? (
+        <Message
+          title="Your favorites list is empty"
+          subtitle="Start exploring and add your first beauty picks 💗"
+          isPadding={false}
+        />
+      ) : (
+        <ProductsList products={favorites} />
+      )}
+    </div>
   );
 };
 
